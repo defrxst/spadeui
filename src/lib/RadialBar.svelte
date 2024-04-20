75388025db4fedb3ab9805b6942759432bfed778
defrost <118262248+defrxst@users.noreply.github.com>
2024-04-19 19:06:32 -0700

    init commit

---
+++ src/lib/RadialBar.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../app.css'
+</script>
+
+<svg width='100' height='100'>
+    <circle cx='50' cy='50' r='40' class='stroke-black fill-white stroke-[7px]'/>
+    <text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle'>70%</text>
+    <circle cx='50' cy='50' r='40' class='stroke-green-500 stroke-[7px]' stroke-dasharray='10' stroke-dashoffset='-60'></circle>
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import RadialBar from "$lib/RadialBar.svelte";
+
+</script>
+<RadialBar></RadialBar>
